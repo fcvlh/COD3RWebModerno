7fc532bb3ae0aa8a53d76e1587036569554bce8c
fcvlh <felipecarvalho4@hotmail.com>
2020-03-30 22:40:56 -0300

    Ajax - XMLHttpRequest e Fetch

---
+++ exercicios-js/ajax/server.js
@@ -0,0 +1,44 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/* UPLOAD */
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload');
+    },
+    filename: function (req, file, callback) {
+        console.log(file);
+        console.log(callback);
+        callback(null, `${Date.now()}_${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage }).single('arquivo');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.');
+        }
+        res.end('Concluído com sucesso!')
+    });
+});
+/* UPLOAD */
+
+/* FETCH API */
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+});
+/* FETCH API */
+
+app.listen(8080, () => console.log('Executando...'));
